c81ab45929d11d7aa8467d7bf20fa03eda48d467
bvlaming <29951819+bvlaming@users.noreply.github.com>
2023-12-01 16:39:38 +0100

    solution day 1

---
+++ day1.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+def filter_string(line: str) -> int:
+    """Filter out non-numerical values and return first and last number."""
+    filtered_string = [c for c in line if c.isdigit()]
+    return int(filtered_string[0] + filtered_string[-1])
+
+
+def filter_string2b(line:str) -> int:
+    digit_map = {"one": "on1ne",
+                 "two": "tw2wo",
+                  "three": "th3ee", "four":"fo4ur",
+                 "five":"fi5ve",
+                 "six":"si6",
+                 "seven":"se7en",
+                 "eight":"ei8ght",
+                 "nine":"ni9ne"}
+    for k, v in digit_map.items():
+        line = line.replace(k, str(v))
+    filtered_string = [c for c in line if c.isdigit()]
+    return int(filtered_string[0] + filtered_string[-1])
+
+def main():
+    with open("data/day1.csv", "r") as file:
+        data = file.read().splitlines()
+
+    filtered_data = [filter_string(line) for line in data]
+    result1a = sum(filtered_data)
+    print(result1a)
+
+    filtered_data2 = [filter_string2b(line) for line in data]
+    print(sum(filtered_data2))
+
+
+if __name__ == "__main__":
+    main()
