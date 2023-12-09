122045d24415445ec09f16463854f450b954cb93
bvlaming <29951819+bvlaming@users.noreply.github.com>
2023-12-09 10:05:39 +0100

    day 7 linting

--- day7.py
+++ day7.py
@@ -1,5 +1,7 @@
 from enum import Enum
 from collections import Counter
+
+
 class Hand(Enum):
     HIGH = "1high"
     PAIR = "2pair"
@@ -10,11 +12,24 @@ class Hand(Enum):
     FIVE = "7five"
 
 
-def score(hand: Hand, card1: int, card2: int=0, card3: int=0, card4: int=0, card5:int=0) -> int:
+def score(
+    hand: Hand,
+    card1: int,
+    card2: int = 0,
+    card3: int = 0,
+    card4: int = 0,
+    card5: int = 0,
+) -> int:
     score_hand = int(hand.value[0])
     return (
-        score_hand*10000000 + card1*20*20*20*20 + card2*20*20*20 + card3*20*20 + card4*20 + card5
+        score_hand * 10000000
+        + card1 * 20 * 20 * 20 * 20
+        + card2 * 20 * 20 * 20
+        + card3 * 20 * 20
+        + card4 * 20
+        + card5
     )
+
 
 def find_hand(cards: list[int]) -> Hand:
     mc = Counter(cards).most_common()
@@ -37,29 +52,26 @@ def find_hand(cards: list[int]) -> Hand:
         hand = Hand.HIGH
     return hand
 
+
 def score_cards(cards_str: str, uses_joker: bool = False) -> int:
     # determine hand, high card, second high card, third high card, 4 high, 5 high
     if uses_joker:
-        int_map = {'T': '10', 'J': '1', 'Q': '12', 'K': '13', 'A': '14'}
+        int_map = {"T": "10", "J": "1", "Q": "12", "K": "13", "A": "14"}
     else:
-        int_map = {'T': '10', 'J': '11', 'Q': '12', 'K': '13', 'A': '14'}
+        int_map = {"T": "10", "J": "11", "Q": "12", "K": "13", "A": "14"}
 
-    cards = [int(int_map.get(c,c)) for c in cards_str]
+    cards = [int(int_map.get(c, c)) for c in cards_str]
     if any(c == 1 for c in cards) and uses_joker:
         if all(c == 1 for c in cards):
             # if all J: transfer to A
-            cards_with_joker = [14]*5
+            cards_with_joker = [14] * 5
         else:
             # add J to the most frequent card
-            mc_type = Counter([c for c in cards if c!=1]).most_common()[0][0]
+            mc_type = Counter([c for c in cards if c != 1]).most_common()[0][0]
             cards_with_joker = [c if c != 1 else mc_type for c in cards]
-            # print(mc_type)
-            # print(cards_with_joker)
         hand = find_hand(cards_with_joker)
-        # print(hand)
     else:
         hand = find_hand(cards)
-    # print(hand)
     card1, card2, card3, card4, card5 = cards[0], cards[1], cards[2], cards[3], cards[4]
     return score(hand, card1, card2, card3, card4, card5)
 
@@ -67,7 +79,12 @@ def score_cards(cards_str: str, uses_joker: bool = False) -> int:
 def parse_data(data: list[str], uses_joker: bool) -> [list[tuple[int, int]]]:
     """pair: 100000. two pair: 200000. 3oak: 3000."""
     return [
-        (score_cards(line.split()[0], uses_joker), int(line.split()[1]), line.split()[0]) for line in data
+        (
+            score_cards(line.split()[0], uses_joker),
+            int(line.split()[1]),
+            line.split()[0],
+        )
+        for line in data
     ]
 
 
@@ -80,12 +97,14 @@ def total_score(data: list[str], uses_joker: bool):
         score += n * y[1]
     return score
 
+
 def main():
     with open("data/day7.csv", "r") as file:
         data = file.read().splitlines()
-    # data = ['J28TQ  666']
+
     print(total_score(data, uses_joker=False))
     print(total_score(data, uses_joker=True))
+
 
 # 249748283
 if __name__ == "__main__":
